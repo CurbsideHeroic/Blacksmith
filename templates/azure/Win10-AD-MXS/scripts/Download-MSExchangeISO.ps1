c633b6b42549d2f8d43fb46954058e1243060acf
CurbsideHeroic <45704993+CurbsideHeroic@users.noreply.github.com>
2022-09-29 16:16:58 -0400

    Added Exchange 2019

--- templates/azure/Win10-AD-MXS/scripts/Download-MSExchangeISO.ps1
+++ templates/azure/Win10-AD-MXS/scripts/Download-MSExchangeISO.ps1
@@ -3,8 +3,8 @@
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$true)]
-    [ValidateSet('MXS2016-x64-CU19-KB4588884','MXS2016-x64-CU18-KB4571788','MXS2016-x64-CU17-KB4556414','MXS2016-x64-CU16-KB4537678','MXS2016-x64-CU15-KB4522150','MXS2016-x64-CU14-KB4514140','MXS2016-x64-CU13-KB4488406','MXS2016-x64-CU12-KB4471392')]
-    [string] $MXSRelease = 'MXS2016-x64-CU19-KB4588884',
+    [ValidateSet('MXS2019-x64-CU12-KB5011156','MXS2016-x64-CU19-KB4588884','MXS2016-x64-CU18-KB4571788','MXS2016-x64-CU17-KB4556414','MXS2016-x64-CU16-KB4537678','MXS2016-x64-CU15-KB4522150','MXS2016-x64-CU14-KB4514140','MXS2016-x64-CU13-KB4488406','MXS2016-x64-CU12-KB4471392')]
+    [string] $MXSRelease = 'MXS2019-x64-CU12-KB5011156',
 
     [Parameter(Mandatory=$true)]
     [ValidateScript({
@@ -30,6 +30,7 @@ $MXSReleaseDownloadUri = Switch ($MXSRelease) {
     'MXS2016-x64-CU14-KB4514140' { 'https://download.microsoft.com/download/f/4/e/f4e4b3a0-925b-4eff-8cc7-8b5932d75b49/ExchangeServer2016-x64-cu14.iso' }
     'MXS2016-x64-CU13-KB4488406' { 'https://download.microsoft.com/download/5/9/6/59681DAE-AB62-4854-8DEC-CA25FFEFE3B3/ExchangeServer2016-x64-cu13.iso' }
     'MXS2016-x64-CU12-KB4471392' { 'https://download.microsoft.com/download/2/5/8/258D30CF-CA4C-433A-A618-FB7E6BCC4EEE/ExchangeServer2016-x64-cu12.iso' }
+    'MXS2019-x64-CU12-KB5011156' { 'https://download.microsoft.com/download/b/c/7/bc766694-8398-4258-8e1e-ce4ddb9b3f7d/ExchangeServer2019-x64-CU12.ISO'}
 }
 
 ################
